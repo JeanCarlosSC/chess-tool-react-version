714c8065a53438ca876d8efd74500ded8c1a6abd
JeanCarlosSC <jeancarlosodi@gmail.com>
2024-07-03 16:36:34 -0500

    creating form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Move types
+const FORCED = 0;
+const BLUNDER = 1;
+const MISS = 2;
+const MISTAKE = 3;
+const INACCURACY = 4;
+const BOOK = 5;
+const GOOD = 6;
+const EXCELLENT = 7;
+const BEST = 8;
+const GREAT = 9;
+const BRILLIANT = 10;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Estas son las opciones disponibles:</p>
+        <p className="opciones"></p>
+
+        <form action="/submit-form" method="post">
+          <label>Ingrese movimiento jugado</label>
+          <textarea
+            id="message"
+            className="App-textarea"
+            name="message"
+            rows="4"
+            cols="50"
+          ></textarea>
+          <input type="submit" value="Enviar"></input>
+        </form>
       </header>
     </div>
   );
